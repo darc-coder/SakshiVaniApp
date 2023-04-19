1fd893d89b29f64eca7e8e6c8e5ffed0c73d9687
Nitin <akashlakra789@gmail.com>
2023-04-19 17:54:54 +0530

    ad completed

--- components/BannerAd.js
+++ components/BannerAd.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
 
 const adUnitId = __DEV__ ? TestIds.BANNER : 'ca-app-pub-6000544536655422/7162720209';
 
 export default function BannerAdComponent() {
   return (
-    // <View style={{height: 60}}>
+    <View style={styles.adBox}>
       <BannerAd
           unitId={adUnitId}
-          size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+          size={BannerAdSize.BANNER}
           requestOptions={{
               requestNonPersonalizedAdsOnly: true,
           }}
       />
-    // </View>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  adBox: {
+    height: 60,
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  }
+})
