786cf4a89e099199a4bad3fbc77cb00bbe469af1
Nitin Lakra <akashlakra789@gmail.com>
2023-04-20 18:15:57 +0530

    commit experimental

--- components/BannerAd.js
+++ components/BannerAd.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
+import { useTheme } from 'react-native-paper';
 
 const adUnitId = __DEV__ ? TestIds.BANNER : 'ca-app-pub-6000544536655422/7162720209';
 
 export default function BannerAdComponent() {
+  const Theme = useTheme();
   return (
-    <View style={styles.adBox}>
+    <View style={[styles.adBox, { backgroundColor: Theme.colors.background } ]}>
       <BannerAd
           unitId={adUnitId}
           size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
